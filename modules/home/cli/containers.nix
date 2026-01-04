fcedee2cfbc3b265a4f2d6e9c7ded08b56625381
m02uku <m02ukuzrzr@gmail.com>
2026-01-04 15:03:00 +0900

    Restore docker package for CLI functionality

--- modules/home/cli/containers.nix
+++ modules/home/cli/containers.nix
@@ -3,6 +3,7 @@
   flake.modules.homeManager.containers = { pkgs, ... }: {
     home.packages = with pkgs; [
       colima    # Docker on macOS/Linux without Docker Desktop
+      docker    # Docker CLI (colima provides the daemon)
     ];
   };
 }
