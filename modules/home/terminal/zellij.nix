920d14f34ae9b6c58bd2e240b4aa5fa09eba2830
m02uku <m02ukuzrzr@gmail.com>
2026-01-05 02:59:47 +0900

    Make zellij copy/paste cross-platform (macOS: pbcopy, Linux: xclip)

--- modules/home/terminal/zellij.nix
+++ modules/home/terminal/zellij.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   flake.modules.homeManager.zellij = { pkgs, ... }: {
-    home.packages = [ pkgs.zellij ];
+    home.packages = [ pkgs.zellij ] ++ (if pkgs.stdenv.isLinux then [ pkgs.xclip ] else []);
     home.file.".config/zellij/layouts/dev.kdl".text = ''
       layout {
         pane split_direction="vertical" {
@@ -18,8 +18,13 @@
     home.file.".config/zellij/config.kdl".text = ''
       default_mode "locked"
       default_shell "zsh"
+      ${if pkgs.stdenv.isDarwin then ''
       copy_command "pbcopy"
       paste_command "pbpaste"
+      '' else ''
+      copy_command "xclip -selection clipboard"
+      paste_command "xclip -selection clipboard -o"
+      ''}
       keybinds {
         locked {
           bind "Ctrl g" { SwitchToMode "Normal"; }
