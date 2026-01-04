b59dfdb00cb14c15be643f0fa1cf455a341f4821
m02uku <m02ukuzrzr@gmail.com>
2026-01-04 14:49:26 +0900

    Refine WezTerm key bindings

--- modules/home/terminal/wezterm.nix
+++ modules/home/terminal/wezterm.nix
@@ -12,35 +12,19 @@
 
         -- Appearance
         config.color_scheme = "Dracula"
-        config.window_background_opacity = 0.85
+        config.window_background_opacity = 0.8
         config.window_decorations = "RESIZE"
         config.hide_tab_bar_if_only_one_tab = true
 
-        -- macOS specific blur
-        if wezterm.target_triple:find("darwin") then
-          config.macos_window_background_blur = 10
-        end
-
-        -- Cursor
-        -- config.default_cursor_style = "SteadyBar"
+        config.macos_window_background_blur = 10
 
         -- Keys
-        config.keys = {}
-
-        -- Platform-specific key bindings
-        local mod_single, mod_double
-        if wezterm.target_triple:find("darwin") then
-          mod_single = "CMD"
-          mod_double = "CMD|SHIFT"
-        else
-          mod_single = "CTRL"
-          mod_double = "CTRL|SHIFT"
-        end
-
-        table.insert(config.keys, { key = "d", mods = mod_single, action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } })
-        table.insert(config.keys, { key = "d", mods = mod_double, action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } })
-        table.insert(config.keys, { key = "w", mods = mod_single, action = wezterm.action.CloseCurrentPane { confirm = true } })
-        table.insert(config.keys, { key = "f", mods = mod_double, action = wezterm.action.ToggleFullScreen })
+        config.keys = {
+          { key = "d", mods = "CTRL", action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
+          { key = "d", mods = "CTRL|SHIFT", action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
+          { key = "w", mods = "CTRL", action = wezterm.action.CloseCurrentPane { confirm = true } },
+          { key = "f", mods = "CTRL|SHIFT", action = wezterm.action.ToggleFullScreen },
+        }
 
         return config
       '';
