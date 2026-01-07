a38bd14137415ed4c03f8b8b59ed48273d9480f8
s0r4d3v <s0r4d3v@gmail.com>
2026-01-08 01:43:07 +0900

    Add comma tool to utils for running commands without installing

--- modules/home/cli/utils.nix
+++ modules/home/cli/utils.nix
@@ -1,27 +1,30 @@
 { ... }:
 {
-  flake.modules.homeManager.utils = { pkgs, ... }: {
-    home.packages = with pkgs; [
-      # Modern ls/cat
-      eza
-      bat
+  flake.modules.homeManager.utils =
+    { pkgs, ... }:
+    {
+      home.packages = with pkgs; [
+        # Modern ls/cat
+        eza
+        bat
 
-      # JSON/HTTP
-      jq
-      curl
-      wget
-      xh           # Modern curl for APIs
+        # JSON/HTTP
+        jq
+        curl
+        wget
+        xh # Modern curl for APIs
 
-      # System monitoring
-      btop         # Modern htop
-      dust         # Modern du
-      duf          # Modern df
+        # System monitoring
+        btop # Modern htop
+        dust # Modern du
+        duf # Modern df
 
-      # Productivity
-      tldr         # Simplified man pages
-      trash-cli    # Safe rm
-      entr         # Run command on file change
-      sshfs        # Mount remote directories over SSH
-    ];
-  };
+        # Productivity
+        tldr # Simplified man pages
+        trash-cli # Safe rm
+        entr # Run command on file change
+        sshfs # Mount remote directories over SSH
+        comma # Run commands without installing
+      ];
+    };
 }
