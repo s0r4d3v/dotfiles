06351852a2f9eea69822207063afbf02f1419bb1
s0r4d3v <s0r4d3v@gmail.com>
2026-01-08 21:49:12 +0900

    Update home.stateVersion to 25.11

--- modules/home/base.nix
+++ modules/home/base.nix
@@ -1,12 +1,14 @@
 { ... }:
 {
-  flake.modules.homeManager.base = { user, homeDir, ... }: {
-    home.username = user;
-    home.homeDirectory = homeDir;
-    home.stateVersion = "25.05";
-    programs.home-manager.enable = true;
-    home.sessionVariables = {
-      DOTFILES_PATH = "${homeDir}/ghq/github.com/s0r4d3v/dotfiles";
+  flake.modules.homeManager.base =
+    { user, homeDir, ... }:
+    {
+      home.username = user;
+      home.homeDirectory = homeDir;
+      home.stateVersion = "25.11";
+      programs.home-manager.enable = true;
+      home.sessionVariables = {
+        DOTFILES_PATH = "${homeDir}/ghq/github.com/s0r4d3v/dotfiles";
+      };
     };
-  };
 }
