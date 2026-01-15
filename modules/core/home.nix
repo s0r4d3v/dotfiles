57a6ccce23682b1adbe93ed7e816e9f99954e32e
s0r4d3v <s0r4d3v@gmail.com>
2026-01-15 23:29:56 +0900

    simplify home.nix

--- modules/core/home.nix
+++ modules/core/home.nix
@@ -1,42 +1,36 @@
 { inputs, config, ... }:
 let
   hmModules = builtins.attrValues config.flake.modules.homeManager;
-  users = [ "soranagano" ];
+  users = [ "soranagano" ]; # Add more users here for multi-machine support
 in
 {
   perSystem =
     { system, ... }:
     let
-      homeConfigs = builtins.listToAttrs (
-        map (
-          userName:
-          let
+      homeConfigurations = builtins.listToAttrs (
+        map (userName: {
+          name = userName;
+          value = inputs.home-manager.lib.homeManagerConfiguration {
             pkgs = import inputs.nixpkgs { inherit system; };
-            nurPkgs = import inputs.nixpkgs {
-              inherit system;
-              overlays = [ inputs.nur.overlays.default ];
-            };
-            homeDir = if pkgs.stdenv.isDarwin then "/Users/${userName}" else "/home/${userName}";
-          in
-          {
-            name = userName;
-            value = inputs.home-manager.lib.homeManagerConfiguration {
-              inherit pkgs;
-              extraSpecialArgs = {
-                user = userName;
-                inherit homeDir nurPkgs;
+            extraSpecialArgs = {
+              user = userName;
+              homeDir =
+                if (import inputs.nixpkgs { inherit system; }).stdenv.isDarwin then
+                  "/Users/${userName}"
+                else
+                  "/home/${userName}";
+              nurPkgs = import inputs.nixpkgs {
+                inherit system;
+                overlays = [ inputs.nur.overlays.default ];
               };
-              modules = [
-                inputs.nixvim.homeModules.nixvim
-              ]
-              ++ hmModules;
             };
-          }
-        ) users
+            modules = [ inputs.nixvim.homeModules.nixvim ] ++ hmModules;
+          };
+        }) users
       );
     in
     {
-      legacyPackages.homeConfigurations = homeConfigs;
-      packages = builtins.mapAttrs (_: config: config.activationPackage) homeConfigs;
+      legacyPackages.homeConfigurations = homeConfigurations;
+      packages = builtins.mapAttrs (_: config: config.activationPackage) homeConfigurations;
     };
 }
